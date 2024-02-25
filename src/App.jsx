af3d0724acdfc5049a5eaaa31f5b021cec449426
nhc2203 <swordartonline123kiritoxasuna@gmail.com>
2024-02-25 22:11:21 +0700

    Dark light p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
 import { Button } from '@mui/material'
 import Typography from '@mui/material/Typography'
+import {useColorScheme} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle/>
       <div>NguyenHungCuong</div>
 
       <Typography variant='body2' color="text.secondary">Typography</Typography>
